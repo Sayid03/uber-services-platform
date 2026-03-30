6c304c59ae3b709d00b14229b15b9295f85b2cc2
Sayid03 <egamberdiefsayid@gmail.com>
2026-03-30 18:48:52 +0500

    Registering Apps

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest-framework',
+
+    'users',
+    'services',
+    'bookings',
+    'reviews',
 ]
 
 MIDDLEWARE = [
