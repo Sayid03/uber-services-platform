3f8577bd3da36e215a0f941443f4ce0eda45443f
Sayid03 <egamberdiefsayid@gmail.com>
2026-04-07 22:15:37 +0500

    Adding User > Views

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import ProviderProfile
+from .serializers import RegisterSerializer, UserSerializer, ProviderProfileSerializer
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class MeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+class ProviderProfileUpdateView(generics.RetrieveUpdateAPIView):
+    serializer_class = ProviderProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        user = self.request.user
+
+        if user.role != 'provider':
+            raise permissions.PermissionDenied('Only providers can access this profile.')
+
+        return user.provider_profile
