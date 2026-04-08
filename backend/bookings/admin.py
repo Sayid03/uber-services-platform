c0ad3ab512619f1a741402cfd4ef163df0614a01
Sayid03 <egamberdiefsayid@gmail.com>
2026-04-08 15:03:11 +0500

    Registering Booking Model in Admin

--- backend/bookings/admin.py
+++ backend/bookings/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'service',
+        'customer',
+        'provider',
+        'booking_date',
+        'status',
+        'estimated_price',
+        'created_at',
+    )
+    list_filter = ('status', 'booking_date', 'created_at')
+    search_fields = (
+        'service__title',
+        'customer__username',
+        'provider__username',
+        'address',
+    )
