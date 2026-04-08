c6cf5b00f8b693d6d7480206ab1c7743059b9a9b
Sayid03 <egamberdiefsayid@gmail.com>
2026-04-08 15:32:30 +0500

    Registering Review Model in Admin

--- backend/reviews/admin.py
+++ backend/reviews/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'booking',
+        'customer',
+        'provider',
+        'service',
+        'rating',
+        'created_at',
+    )
+    list_filter = ('rating', 'created_at')
+    search_fields = (
+        'customer__username',
+        'provider__username',
+        'service__title',
+        'comment',
+    )
